3bfc7be49a34413d1b72ee59ca22220e9e6063b1
Ilya Dynin <72310394+ilyadynin@users.noreply.github.com>
2023-12-11 01:14:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Juhu, Prinzessin, dann sehen wir uns bald ";
+  question.innerHTML = "Juhu, Prinzessin, dann bis Freitag!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
